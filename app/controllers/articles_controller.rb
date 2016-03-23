9e04ef59f1701d43298d91cf95d80dbfc0f15296
Rails Udemy Course <railsudemycourse@gmail.com>
2016-03-23 22:59:13 +0000

    create new template,action and route

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,19 +3,25 @@ class ArticlesController < ApplicationController
  @article = Article.new
   end
 
+
  def create
-
-   @articles =Article.new(article-params)
+   @article = Article.new(article_params)
    @article.save
-
-
-
+   redirect_to articles_show(@article)
  end
+
 
   private
   def article_params
     params.require(:article).permit(:title, :description)
 
   end
+ end
 
-end
+
+
+
+
+
+
+
